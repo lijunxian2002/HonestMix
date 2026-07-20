ef016a71d1fda04acdd4996a74eb0fdebf4d044f
lijunxian2002 <lijunxian2002@github.com>
2026-07-21 06:34:20 +0800

    tools目录加入gitignore

--- tools/build_fir.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "D:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
-cl /nologo /O2 /std:c++17 /Fe:C:\Users\Administrator\source\repos\HonestMix\tools\export_fir.exe C:\Users\Administrator\source\repos\HonestMix\tools\export_fir.cpp
-C:\Users\Administrator\source\repos\HonestMix\tools\export_fir.exe > C:\Users\Administrator\source\repos\HonestMix\Source\dsp\fir_data.h
-echo DONE
